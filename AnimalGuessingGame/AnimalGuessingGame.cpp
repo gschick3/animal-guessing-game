0f59c0d187ed04b04122870f76ab995432ea2276
gschick3 <gschick3@gmail.com>
2022-04-16 13:32:18 -0500

    improve UX and combine input/output files

--- AnimalGuessingGame/AnimalGuessingGame.cpp
+++ AnimalGuessingGame/AnimalGuessingGame.cpp
@@ -9,12 +9,19 @@ int main()
 {
     AnimalTree tree;
 
-    if (!tree.load("animals.txt")) {
+    string filename = "animals.txt";
+
+    if (!tree.load(filename)) {
         cout << "Error loading animal file." << endl;
         return 1;
     }
 
+    cout << "Welcome to the Animal Guessing Game" << endl;
+    cout << "I will ask you a series of questions to determine what animal you're thinking of." << endl;
+    cout << "Only answer y or n to each." << endl;
+    cout << endl;
 
+    string ws; // for whitespace
     char yn = 'y';
     while (yn == 'y') { // game loop
         tree.reset();
@@ -22,9 +29,11 @@ int main()
 
         while (true) { // round loopt
             cout << tree.getNextQA() << endl; // print either animal guess or next question
-
-            if (nextIsAnimal) { // if the node is an animal guess
+            
+            // if the node is an animal guess
+            if (nextIsAnimal) { 
                 cin >> yn;
+                getline(cin, ws); // capture whitespace
                 if (yn == 'y') {
                     cout << "Yay!" << endl;
                 }
@@ -32,11 +41,10 @@ int main()
                     string name;
                     string question;
                     char path;
-                    cout << "What was your animal?" << endl;
-                    cin >> name;
-                    getline(cin, question); // get whitespace
+                    cout << "What was your animal? (Enter only the name in lowercase)" << endl;
+                    getline(cin, name);
 
-                    cout << "What yes/no question would differentiate it from a(n) " << tree.getNext()->getQA() << "?" << endl;
+                    cout << "What yes/no question would differentiate it from a(n) " << tree.getNext()->getQA() << "? (Enter a question with question mark)" << endl;
                     getline(cin, question);
                     cout << "Would the answer be 'y' or 'n' for the new animal?" << endl;
                     cin >> path;
@@ -56,5 +64,5 @@ int main()
     cout << "Would you like to save the current tree?" << endl;
     cin >> yn;
     if (yn == 'y')
-        tree.saveToFile("output.txt");
+        tree.saveToFile(filename);
 }
